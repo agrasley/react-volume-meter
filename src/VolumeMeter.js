41ad1269d7351cc1128f9c7fddda8cac5d3ce725
Alex Grasley <alex@grasley.com>
2016-06-07 17:42:26 -0700

    fast attack slow release

--- src/VolumeMeter.js
+++ src/VolumeMeter.js
@@ -2,15 +2,17 @@
 
 import React, { PropTypes } from 'react'
 
-const draw = (width, height, canvasCtx, volume) => {
+const draw = (width, height, canvasCtx, prevVolume, volume, maxVolume) => {
+  const vol = Math.max(volume, prevVolume * 0.95)
   canvasCtx.clearRect(0, 0, width, height)
   for (let i = 0; i < 5; i++) {
-    canvasCtx.fillStyle = ((i + 1) * 10 < volume) ? 'green' : 'grey'
+    canvasCtx.fillStyle = ((i + 1) * (maxVolume / 5) < vol) ? 'green' : 'grey'
     const x = width * i / 5
     const y = height * 0.6 - height * i * 0.15
     canvasCtx.fillRect(x, y, width / 6, height - y)
-    if (i * 10 < volume) {
-      canvasCtx.fillRect(x, y, ((volume % 10) / 10) * (width / 6), height - y)
+    if (i * 10 < vol) {
+      canvasCtx.fillStyle = 'green'
+      canvasCtx.fillRect(x, y, ((vol % (maxVolume / 5)) / (maxVolume / 5)) * (width / 6), height - y)
     }
   }
 }
@@ -28,12 +30,15 @@ const VolumeMeter = React.createClass({
   },
 
   start () {
-    const { width, height } = this.props
+    const { width, height, maxVolume } = this.props
     const canvasCtx = this.refs.canvas.getContext('2d')
+    let prevVolume = 0
 
     const drawLoop = () => {
       if (this.analyser.ended) return
-      draw(width, height, canvasCtx, this.getVolume())
+      const volume = this.getVolume()
+      draw(width, height, canvasCtx, volume, prevVolume, maxVolume || 50)
+      prevVolume = volume
       this.rafId = window.requestAnimationFrame(drawLoop)
     }
 
@@ -41,17 +46,17 @@ const VolumeMeter = React.createClass({
   },
 
   stop () {
-    const { width, height } = this.props
+    const { width, height, maxVolume } = this.props
     const canvasCtx = this.refs.canvas.getContext('2d')
     window.cancelAnimationFrame(this.rafId)
-    draw(width, height, canvasCtx, 0)
+    draw(width, height, canvasCtx, 0, 0, maxVolume || 50)
   },
 
   componentDidMount () {
-    const { width, height } = this.props
+    const { width, height, maxVolume } = this.props
     const canvasCtx = this.refs.canvas.getContext('2d')
 
-    draw(width, height, canvasCtx, 0)
+    draw(width, height, canvasCtx, 0, 0, maxVolume || 50)
   },
 
   componentWillUpdate (nextProps) {
@@ -89,7 +94,8 @@ const VolumeMeter = React.createClass({
     audioContext: PropTypes.object.isRequired,
     src: PropTypes.object,
     width: PropTypes.number.isRequired,
-    height: PropTypes.number.isRequired
+    height: PropTypes.number.isRequired,
+    maxVolume: PropTypes.number
   }
 })
 
